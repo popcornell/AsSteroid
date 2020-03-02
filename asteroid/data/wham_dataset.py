4a4c2bfbcbc5f7c5390fc5567bf0bf5eba61fd68
popcornell <cornellsamuele@gmail.com>
2020-03-02 19:37:42 +0100

    data_augmentation for wham need review !

--- asteroid/data/wham_dataset.py
+++ asteroid/data/wham_dataset.py
@@ -4,6 +4,7 @@ import json
 import os
 import numpy as np
 import soundfile as sf
+import glob
 
 DATASET = 'WHAM'
 # WHAM tasks
@@ -147,3 +148,145 @@ class WhamDataset(data.Dataset):
             source_arrays.append(s)
         sources = torch.from_numpy(np.vstack(source_arrays))
         return torch.from_numpy(x), sources
+
+
+class AugmentedWhamDataset(data.Dataset):
+    """ Dataset class for WHAM source separation and speech enhancement tasks.
+
+    Args:
+        wsj_train_dir (str): The path to the directory containing the wsj train/dev/test .wav files.
+        task (str): One of ``'enh_single'``, ``'enh_both'``, ``'sep_clean'`` or
+            ``'sep_noisy'``.
+
+            * ``'enh_single'`` for single speaker speech enhancement.
+            * ``'enh_both'`` for multi speaker speech enhancement.
+            * ``'sep_clean'`` for two-speaker clean source separation.
+            * ``'sep_noisy'`` for two-speaker noisy source separation.
+        noise_dir (str, optional): The path to the directory containing the WHAM train/dev/test .wav files
+        sample_rate (int, optional): The sampling rate of the wav files.
+        segment (float, optional): Length of the segments used for training,
+            in seconds. If None, use full utterances (e.g. for test).
+        nondefault_nsrc (int, optional): Number of sources in the training
+            targets.
+            If None, defaults to one for enhancement tasks and two for
+            separation tasks.
+        random_gain: (tuple, optional): Minimum and maximum bounds for mix energy (dB).
+        max_db_diff: (tuple, optional): Minimum and maximum bounds
+                                        for difference in dB between one of two sources in the mix
+    """
+    def __init__(self, wsj_train_dir, task, noise_dir=None, sample_rate=8000, segment=4.0,
+                 nondefault_nsrc=None, random_gain=(-12, 0), max_db_diff=(-6, 0)):
+        super(AugmentedWhamDataset, self).__init__()
+        if task not in WHAM_TASKS.keys():
+            raise ValueError('Unexpected task {}, expected one of '
+                             '{}'.format(task, WHAM_TASKS.keys()))
+        # Task setting
+        self.task = task
+        if self.task in ["sep_noisy", "enh_single"] and not noise_dir:
+            raise RuntimeError("noise directory must be specified if task is sep_noisy or enh_single")
+        self.task_dict = WHAM_TASKS[task]
+        self.sample_rate = sample_rate
+        self.seg_len = None if segment is None else int(segment * sample_rate)
+        self.random_gain = random_gain
+        self.max_db_diff = max_db_diff
+        if not nondefault_nsrc:
+            self.n_src = self.task_dict['default_nsrc']
+        else:
+            assert nondefault_nsrc >= self.task_dict['default_nsrc']
+            self.n_src = nondefault_nsrc
+        self.like_test = self.seg_len is None
+        # Load json files
+        utterances = glob.glob(os.path.join(wsj_train_dir, "*.wav"))
+        noises = None
+        if self.task in ["sep_clean", "enh_clean"]:
+            noises = glob.glob(os.path.join(wsj_train_dir, "*.wav"))
+
+        # parse utterances according to speaker
+        drop_utt, drop_len = 0, 0
+        print("Parsing WSJ speakers")
+        examples_hashtab = {}
+        for utt in utterances:
+            # exclude if too short
+            c_len = len(sf.SoundFile(utt))
+            if  c_len < self.seg_len:
+                drop_utt += 1
+                drop_len += c_len
+                continue
+            speaker = utt.split("/")[-2]
+            if speaker not in examples_hashtab.keys():
+                examples_hashtab[speaker] = [(utt, c_len)]
+            else:
+                examples_hashtab[speaker].append((utt, c_len))
+
+        print("Drop {} utts({:.2f} h) from {} (shorter than {} samples)".format(
+            drop_utt, drop_len / sample_rate / 36000, len(utterances), self.seg_len))
+
+        examples_hashtab = {"noise": []} # not  bug will use this in __getitem__
+
+        drop_utt, drop_len = 0, 0
+        if noises:
+            for noise in noises:
+                c_len = len(sf.SoundFile(noise))
+                if c_len < self.seg_len:
+                    drop_utt += 1
+                    drop_len += c_len
+                    continue
+                examples_hashtab["noise"].append((noise, c_len))
+
+        print("Drop {} noises({:.2f} h) from {} (shorter than {} samples)".format(
+            drop_utt, drop_len / sample_rate / 36000, len(noises), self.seg_len))
+
+        self.examples = examples_hashtab
+
+    def __add__(self, wham):
+
+        raise NotImplementedError # TODO It will require different handling of other datasets, i suggets using dicts
+
+    def __len__(self):
+
+        return min([len(self.examples[x]) for x in self.examples.keys()])
+
+
+    @staticmethod
+    def get_random_subsegment(array, desired_len, tot_length):
+
+        offset = 0
+        if desired_len < tot_length:
+            offset = np.random.randint(0, tot_length - desired_len)
+
+        return array[offset: offset + desired_len]
+
+
+    def __getitem__(self, idx):
+        """ Gets a mixture/sources pair.
+        Returns:
+            mixture, vstack([source_arrays])
+        """
+        # Sample k speakers randomly
+        c_speakers = np.random.choice([x for x in self.examples.keys() if x != "noise"], self.n_src)
+
+        sources = []
+        for i, spk in enumerate(c_speakers):
+            tmp, tmp_spk_len = np.random.choice(self.examples[c_speakers[spk]], 1)
+            tmp = self.get_random_subsegment(tmp, self.seg_len, tmp_spk_len)
+            tmp = tmp / (np.max(np.abs(tmp)) + 1e-7)
+            if i != 0:
+                tmp = tmp* (10 ** (np.random.randint(self.max_db_diff[0], self.max_db_diff[1]) / 20 ) )
+            sources.append(tmp)
+
+        if self.examples["noise"]:
+            # add also noise
+            tmp, tmp_spk_len = np.random.choice(self.examples["noise"], 1)
+            tmp = self.get_random_subsegment(tmp, self.seg_len, tmp_spk_len)
+            tmp = tmp / (np.max(np.abs(tmp)) + 1e-7)
+            tmp = tmp * (10 ** (np.random.randint(self.max_db_diff[0], self.max_db_diff[1]) / 20))
+            sources.append(tmp)
+
+        mix = np.mean(np.stack(sources), 0)
+
+        if self.examples["noise"]:
+            sources = sources[:-1]
+
+        sources = np.stack(sources)
+
+        return torch.from_numpy(mix), sources
